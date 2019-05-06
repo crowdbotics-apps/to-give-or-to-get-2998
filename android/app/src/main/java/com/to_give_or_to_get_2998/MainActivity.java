e001f137cffaf3289fdc858fdf3e9dbb96e7b4da
Crowdbotics <team@crowdbotics.com>
2019-05-06 19:27:17 +0000

    initial commit

---
+++ android/app/src/main/java/com/to_give_or_to_get_2998/MainActivity.java
@@ -0,0 +1,15 @@
+package com.to_give_or_to_get_2998;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "to_give_or_to_get_2998";
+    }
+}
